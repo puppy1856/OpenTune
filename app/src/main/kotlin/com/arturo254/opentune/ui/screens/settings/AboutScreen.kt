1f2255f2cb219931ef442e42b2b4e36fa4781143
Arturo Cervantes <artuscervantes111@gmail.com>
2026-04-18 20:08:31 -0600

    style: remove unused imports in `AboutScreen.kt`
    This commit cleans up the `AboutScreen.kt` file by removing unused imports, including `border`, `width`, and `items`.
    
    Signed-off-by: Arturo Cervantes <artuscervantes111@gmail.com>
    (cherry picked from commit 98d0f98637d2efe71454ce3b7afda72cbf29a3bb)

--- app/src/main/kotlin/com/arturo254/opentune/ui/screens/settings/AboutScreen.kt
+++ app/src/main/kotlin/com/arturo254/opentune/ui/screens/settings/AboutScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -28,10 +27,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
